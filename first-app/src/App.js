acdc5117290d1ba012eab6814a5d60f04a530410
Samvel Asoyan <samvel24.1998@gmail.com>
2019-10-21 21:11:07 +0400

    todo list example

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,38 +1,112 @@
-import React,{Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class List extends Component{
-  state = {
-    value: ""
-  }
-  render(){
-    return (<table className="dropDown">
-            <tr className="row">
-              <td className="icon"><a href="#"><i className="far fa-circle"></i><i className="far fa-check-circle"></i></a></td>
-              <td className="txt">{this.state.value}</td>
-              <td className="icon"><a href="#"><i className="fas fa-times"></i></a></td>
+function List({ value, deleteTask, completeTask, completed , opened}) {
+    return (
+            <tr className={`row ${completed? "checked" : ""}`}>
+                <td className="icon">
+                    <a href="#" onClick={completeTask}>
+                        <i className="far fa-circle"></i>
+                        <i className="far fa-check-circle"></i>
+                    </a>
+                </td>
+                <td className="txt">{value}</td>
+                <td className="icon">
+                    <a href="#" onClick={deleteTask}>
+                        <i className="fas fa-times"></i>
+                    </a>
+                </td>
             </tr>
-          </table>)
-  }
+        
+    );
 }
 class App extends Component {
-  renderList(){
-    return <List />
-  }
-  render() {
-    return <div id="list">
-      <table>
-        <tr>
-          <th>Tasks</th>
-          <td>
-            <input type="text" id="createTask" placeholder="create new task"></input>
-            <a href="#" id="addTask"><i class="fas fa-plus-circle"></i></a>
-          </td>
-          <th><a href="#" id="dropList"><i class="fas fa-bars"></i></a></th>
-        </tr>
-      </table>
-    </div>
-  };
+    state = {
+        value: "",
+        list: [],
+        completed: false,
+        opened: true,
+    };
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if(this.state.value !== "") {
+          let list = [{ value: this.state.value, id: Date.now(), completed: this.state.completed }, ...this.state.list];
+          this.setState({
+              list: list,
+              value: ""
+          });}
+    };
+
+    deleteTask = (id) => {
+        let list = this.state.list.filter((item) => item.id !== id);
+        this.setState({ list });
+    };
+
+    completeTask = (id) => {
+        let list = this.state.list
+        list.map((item) => {
+              if(item.id === id){
+              item.completed = !item.completed
+          }
+        })
+        this.setState({ list });
+    };
+
+    openList = () => {
+      this.setState({opened: !this.state.opened})
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div id="list">
+                    <table>
+                        <tr>
+                            <th>Tasks</th>
+                            <td>
+                                <input
+                                    value={this.state.value}
+                                    onChange={this.handleChange}
+                                    type="text"
+                                    id="createTask"
+                                    placeholder="create new task"
+                                ></input>
+                                <input type="submit" value="" />
+                                <a href="#" id="addTask">
+                                    <i class="fas fa-plus-circle"></i>
+                                </a>
+                            </td>
+                            <th>
+                                <a href="#" id="dropList" onClick={()=>this.openList()}>
+                                    <i class="fas fa-bars"></i>
+                                </a>
+                            </th>
+                        </tr>
+                    </table>
+                    <table className={`dropDown ${this.state.opened? "drop" : ""}`}>
+                        {this.state.list.map((item, index) => {
+                            return (
+                                <List
+                                    {...item}
+                                    key={index}
+                                    // value={item.value}
+                                    // id={item.id}
+                                    deleteTask={() => this.deleteTask(item.id)}
+                                    completeTask={() => this.completeTask(item.id)}
+                                    // completed={item.completed}
+                                    opened = {this.opened}
+                                />
+                            );
+                        })}
+                    </table>
+                </div>
+            </form>
+        );
+    }
 }
 
 export default App;
